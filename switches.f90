13ed4f030b772723e467a86e9b5ade7685e1d200
cibinjoseph <cibinjoseph92@gmail.com>
2018-07-14 12:44:14 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -17,10 +17,10 @@ integer, parameter :: wakestrain_switch=0
 integer, parameter :: wakeplot_switch=2
 
 ! Wake convection finite-difference scheme [0]FT1 [1]PC2 [2]AB2 [3]CB2D [4]AB4
-integer, parameter :: FDscheme_switch=3
+integer, parameter :: FDscheme_switch=0
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
 integer, parameter :: wake_ignore_nt=0
 
 ! Initial Wake velocity [0]Off [1..]timesteps
-integer, parameter :: init_wake_vel_nt=100
+integer, parameter :: init_wake_vel_nt=0
