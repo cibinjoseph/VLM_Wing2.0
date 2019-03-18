82b9dc06ca6d343774cd59763d1fb67c670cab86
cibinjoseph <cibinjoseph92@gmail.com>
2019-03-18 22:45:46 +0530

    testcase

--- library.f90
+++ library.f90
@@ -5,7 +5,7 @@ module library
   implicit none
 
   ! Input parameters
-  integer, parameter :: nt = 120
+  integer, parameter :: nt = 360
   integer, parameter :: ns = 20
   integer, parameter :: nc = 6
 
@@ -503,7 +503,7 @@ contains
     integer :: ii,jj,ifil
     oseen_param= 1.2564_dp
     kin_visc   = 0.0000181_dp
-    turb_visc  = 1000._dp
+    turb_visc  = 50._dp
 
     do jj=1,size(wake_array,2)
       do ii=1,size(wake_array,1)
